849063f1eb8a95ec6d1e244f180602ab7489930d
nezba00 <nbahou98@gmail.com>
2025-04-25 16:15:07 +0200

    fix: add slurm commands to .sh script

--- run_snakemake_full.sh
+++ run_snakemake_full.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#SBATCH --job-name=snakemake-master
+#SBATCH --time=72:00:00            # wall clock for master process
+#SBATCH --cpus-per-task=8          # lightweight — Snakemake itself is not heavy
+#SBATCH --mem=8G                   
+#SBATCH --output=./slurm_logs/snake.%j.out
+#SBATCH --error=./slurm_logs/snake.%j.err
+
+
+# Initialize mamba/conda
+source /home/nbahou/miniforge3/etc/profile.d/conda.sh
+source /home/nbahou/miniforge3/etc/profile.d/mamba.sh
+
+mamba activate dino_env
 
 # Run Snakemake with cluster submission
 snakemake -s full_pipeline_snakefile all \
@@ -8,9 +21,9 @@ snakemake -s full_pipeline_snakefile all \
   --cores 8 \
   --jobs 40 \
   -k \
-  --cluster "sbatch --time=03:30:00 \
+  --cluster "sbatch --time=48:30:00 \
              --gres=gpu:rtx4090:1 \
-             --cpus-per-task=16 \
+             --cpus-per-task=18 \
              --mem=80GB \
              --output=/home/nbahou/myimaging/scDINO/slurm_logs/slurm_output_%j.txt \
              --error=/home/nbahou/myimaging/scDINO/slurm_logs/slurm_error_%j.txt" \

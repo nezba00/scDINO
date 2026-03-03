2887be523ca654cb2223aa7a6c93056729b24264
nezba00 <nbahou98@gmail.com>
2026-03-03 16:33:59 +0100

    refactor: untrack umap_explorer, move to own repo Removed umap_explorer from version control in this fork. The file will be managed in a dedicated repository as a standalone tool.

--- umap_explorer/app/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Dash web application for UMAP exploration."""
-
-from .app import create_app, state
-
-__all__ = ["create_app", "state"]
--- umap_explorer/explorer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Interactive BioExplorer dashboard."""
-
-from .bio_explorer import BioExplorer
-from .state import ExplorerState
-
-__all__ = ["BioExplorer", "ExplorerState"]

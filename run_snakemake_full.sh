6b987224afceea5fef456702fdc4845f6261c822
nezba00 <nbahou98@gmail.com>
2026-04-14 23:35:57 +0200

    fix: reduce number of parallel jobs to one --> for stability

--- run_snakemake_full.sh
+++ run_snakemake_full.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=snakemake-master
-#SBATCH --time=72:00:00            # wall clock for master process
+#SBATCH --time=190:00:00            # wall clock for master process
 #SBATCH --cpus-per-task=8          # lightweight — Snakemake itself is not heavy
-#SBATCH --mem=8G                   
+#SBATCH --mem=8G
 #SBATCH --output=./slurm_logs/snake.%j.out
 #SBATCH --error=./slurm_logs/snake.%j.err
+##SBATCH --nodelist=izbdhaka
 
 
 # Initialize mamba/conda
 source /home/nbahou/miniforge3/etc/profile.d/conda.sh
 source /home/nbahou/miniforge3/etc/profile.d/mamba.sh
 
-mamba activate dino_env
+mamba activate dino_env #new_scdino
 
 # Run Snakemake with cluster submission
 snakemake -s full_pipeline_snakefile all \
@@ -19,14 +20,17 @@ snakemake -s full_pipeline_snakefile all \
   --keep-incomplete \
   --drop-metadata \
   --cores 8 \
-  --jobs 40 \
+  --jobs 1 \
   -k \
-  --cluster "sbatch --time=48:30:00 \
-             --gres=gpu:rtx4090:1 \
-             --cpus-per-task=18 \
-             --mem=80GB \
+  --cluster "sbatch --time=90:30:00 \
+             --gres=gpu:rtx6000:2 \
+             --cpus-per-task=20 \
+             --mem=100GB \
              --output=/home/nbahou/myimaging/scDINO/slurm_logs/slurm_output_%j.txt \
              --error=/home/nbahou/myimaging/scDINO/slurm_logs/slurm_error_%j.txt" \
-  --latency-wait 90 \
-  --rerun-incomplete \
-  --forceall
+  --latency-wait 300 #\
+  #--rerun-incomplete #\
+  #--forceall \
+  #--verbose \
+  #--unlock #\
+  #--forceall
